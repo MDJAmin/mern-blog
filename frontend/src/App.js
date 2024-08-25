5b2b235ce83d1c065f0078900dd60f851422d782
MDJAmin <mohammadamindf@gmail.com>
2024-08-25 18:28:47 +0430

    Refactor : separate the app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,118 +1,20 @@
 import "./App.css";
+import Posts from "./BlogPost";
+import NavBar from "./NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="#" className="logo">
-          MyBlog
-        </a>
-        <nav>
-          <a href="#">Login</a>
-          <a href="#">Register</a>
-        </nav>
-      </header>
-      <section className="entries">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2024/08/beats-studio-in-brief.jpg?resize=900,502"
-            alt="product"
-          />
-        </div>
-        <div>
-          <h2>
-            Apple is bringing audio sharing to Beats Studio Pro via firmware
-            update
-          </h2>
-          <p className="info">
-          <a className="author">Apple</a>
-          <time>2024-01-01 00:00</time>
-          </p>
-          <p className="summary">
-            Beats Studio Pro are getting one of AirPods’ best features. A
-            firmware update, noted by 9to5Mac, is delivering multi-user audio
-            sharing, allowing music to be streamed to multiple headphones at
-            once. Apple regularly releases features to its AirPods line before
-            they make their way to the Beats brand.
-          </p>
-        </div>
-      </section>
-      <section className="entries">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2024/08/beats-studio-in-brief.jpg?resize=900,502"
-            alt="product"
-          />
-        </div>
-        <div>
-          <h2>
-            Apple is bringing audio sharing to Beats Studio Pro via firmware
-            update
-          </h2>
-          <p className="info">
-          <a className="author">Apple</a>
-          <time>2024-01-01 00:00</time>
-          </p>
-          <p className="summary">
-            Beats Studio Pro are getting one of AirPods’ best features. A
-            firmware update, noted by 9to5Mac, is delivering multi-user audio
-            sharing, allowing music to be streamed to multiple headphones at
-            once. Apple regularly releases features to its AirPods line before
-            they make their way to the Beats brand.
-          </p>
-        </div>
-      </section>
-      <section className="entries">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2024/08/beats-studio-in-brief.jpg?resize=900,502"
-            alt="product"
-          />
-        </div>
-        <div>
-          <h2>
-            Apple is bringing audio sharing to Beats Studio Pro via firmware
-            update
-          </h2>
-          <p className="info">
-          <a className="author">Apple</a>
-          <time>2024-01-01 00:00</time>
-          </p>
-          <p className="summary">
-            Beats Studio Pro are getting one of AirPods’ best features. A
-            firmware update, noted by 9to5Mac, is delivering multi-user audio
-            sharing, allowing music to be streamed to multiple headphones at
-            once. Apple regularly releases features to its AirPods line before
-            they make their way to the Beats brand.
-          </p>
-        </div>
-      </section>
-      <section className="entries">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2024/08/beats-studio-in-brief.jpg?resize=900,502"
-            alt="product"
-          />
-        </div>
-        <div>
-          <h2>
-            Apple is bringing audio sharing to Beats Studio Pro via firmware
-            update
-          </h2>
-          <p className="info">
-          <a className="author">Apple</a>
-          <time>2024-01-01 00:00</time>
-          </p>
-          <p className="summary">
-            Beats Studio Pro are getting one of AirPods’ best features. A
-            firmware update, noted by 9to5Mac, is delivering multi-user audio
-            sharing, allowing music to be streamed to multiple headphones at
-            once. Apple regularly releases features to its AirPods line before
-            they make their way to the Beats brand.
-          </p>
-        </div>
-      </section>
-    </main>
+    <Router>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<p>Login</p>} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
