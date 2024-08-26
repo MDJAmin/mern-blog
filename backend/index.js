30477d521ff9b6fa1e2762a6176878e67e20a6e4
Mohammad Amin Jafarnezhad <mohammadamindf@gmail.com>
2024-08-26 11:02:07 +0330

    Refactor : add user model
    add user model and handle the register process

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,35 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const app = express();
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
 
-app.post("/register",(req,res)=>{
-    res.json("test ok 2")
-})
-app.listen(4000)
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://amin:HkvLRfibimGnPYha@cluster0.sd44t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const salt = bcrypt.genSaltSync(10); 
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const userDoc = await User.create({
+      username,
+      password: hashedPassword,
+    });
+
+    res.status(201).json(userDoc);
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
