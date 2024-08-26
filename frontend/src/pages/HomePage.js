252734ef8de44ddea5584869dea7e2af68ca1412
Mohammad Amin Jafarnezhad <mohammadamindf@gmail.com>
2024-08-26 16:14:40 +0330

    add the post into home page

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,9 +1,45 @@
-import Posts from "../BlogPost";
+import React, { useEffect, useState } from "react";
 
 export default function HomePage() {
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/posts");
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
-    <>
-      <Posts />
-    </>
+    <div className="entries">
+      {posts.length > 0 ? (
+        posts.map((post) => (
+      
+          <div key={post._id} className="post">
+            {post.cover && (
+              <img
+                src={`http://localhost:4000/uploads/${post.cover
+                  .split("/")
+                  .pop()}`}
+                alt={post.title}
+              />
+            )}
+            <h2 className="title">{post.title}</h2>
+            <p className="date-time">{new Date(post.createdAt).toLocaleString()}</p>
+            <p className="summary">{post.summary}</p>
+            <hr/>
+          </div>  
+        ))
+      ) : (
+        <p>No posts available</p>
+      )}
+    </div>
   );
 }
