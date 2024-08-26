a1b9449ee2b1ded5ef0a13e0312f7eed4482865c
MDJAmin <mohammadamindf@gmail.com>
2024-08-26 07:17:28 +0430

    Refactor : add state to register

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const register = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form className="register" action="">
-        <h2>Register</h2>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
+    <form className="register" onSubmit={register} action="">
+      <h2>Register</h2>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button type="submit">Register</button>
     </form>
   );
