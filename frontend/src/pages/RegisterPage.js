e698a321b292ead5dabc1384729541329fda371c
Mohammad Amin Jafarnezhad <mohammadamindf@gmail.com>
2024-08-26 11:00:24 +0330

    Refactor : Update frontend
    handle the error with await and some basic alert that i will fix them later on ...

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -3,9 +3,30 @@ import { useState } from "react";
 export default function RegisterPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const register = (e) => {
+
+  async function register(e) {
     e.preventDefault();
-  };
+    try {
+      const response = await fetch("http://localhost:4000/register", {
+        method: "POST",
+        body: JSON.stringify({ username, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (response.ok) { 
+        const data = await response.json(); 
+        console.log("Registration successful:", data);
+        alert("Registration successful");
+      } else {
+        console.error("Registration failed with status:", response.status);
+        alert("Registration failed!");
+      }
+    } catch (error) {
+      console.error("Fetch error:", error);
+      alert("Failed to register");
+    }
+  }
+
   return (
     <form className="register" onSubmit={register} action="">
       <h2>Register</h2>
