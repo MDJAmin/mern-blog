62fc95b3e0620d35002f86d185a5903875cb2cee
Mohammad Amin Jafarnezhad <mohammadamindf@gmail.com>
2024-08-26 12:37:38 +0330

    Refactor : logout process
    i get the token from the local storage and check if there was any token it will show the logout Link tag to the user and if there was not token if local storage it will show the login and register

---
+++ frontend/src/Components/NavBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function NavBar() {
+  const navigate = useNavigate();
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  const isLoggedIn = !!localStorage.getItem("token");
+
+  return (
+    <header>
+      <Link to="/" className="logo">
+        MyBlog
+      </Link>
+      <nav>
+        {isLoggedIn ? (
+          <>
+            <Link to="/" onClick={handleLogout}>
+              Logout
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/Login">Login</Link>
+            <Link to="/Register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
